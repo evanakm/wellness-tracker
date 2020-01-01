7eecba091744697f6be9c1510996c93374535db2
Evan Meikleham <evanakm@gmail.com>
2020-01-01 11:24:43 -0700

    Cleaning up and packaging for distribution

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     include_package_data=True,
     keywords='wellness_tracker',
     name='wellness_tracker',
-    packages=find_packages(include=['wellness_tracker', 'wellness_tracker.*']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
